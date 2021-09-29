ba24b630fba6714c0cbe7a1ca3cc70ab45338c96
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-09-29 13:54:57 +0200

    CB-1 add bulma amd sass support; test dynamodb

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React, {useState} from 'react';
-import './App.css';
-import { API, graphqlOperation } from "aws-amplify"
-import { createTodo } from "./graphql/mutations"
-
+import React, { useState } from "react";
+import "./App.sass";
+import { API, graphqlOperation } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
 
 function App() {
-
-    const [todoName, setTodoName] = useState('')
+  const [todoName, setTodoName] = useState("");
   const handleChange = (evt) => {
-    setTodoName(evt.target.value)
-  }
+    setTodoName(evt.target.value);
+  };
 
   const addTodo = async () => {
     await API.graphql(
-        graphqlOperation(createTodo, { input: { name: todoName } })
-    )
-    setTodoName('') // make the input blank again
-  }
+      graphqlOperation(createTodo, { input: { name: todoName } })
+    );
 
+    setTodoName(""); // make the input blank again
+  };
 
   return (
-      <div className="App">
-        <input type="text" value={todoName} onChange={handleChange} />
-        <button onClick={addTodo}>Add ToDo</button>
+    <div className="container">
+      <div className="block">
+        <input
+          name="todo"
+          className="input is-primary is-large"
+          type="text"
+          value={todoName}
+          onChange={handleChange}
+          placeholder="Add TODO"
+        />
       </div>
+      <button className="button mt-1" onClick={addTodo}>
+        Add ToDo
+      </button>
+    </div>
   );
 }
 
