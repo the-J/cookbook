e21c7f51331f5b5c5b331b47880ecbc56c995bfa
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-06 20:54:30 +0200

    CB-1 placeholder views

--- src/views/SignUp/SignUp.view.js
+++ src/views/SignUp/SignUp.view.js
@@ -1,5 +1,68 @@
 import React from "react";
 
-const SignUpView = () => <div>SignUp</div>;
+import { useForm } from "react-hook-form";
+import { useAuthContext } from "../../context/auth.context";
+import { signUp } from "../../auth/authUser";
+
+const SignUpView = () => {
+  const auth = useAuthContext();
+  console.log({ auth });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      name: "Name",
+      email: "Email",
+    },
+    criteriaMode: "all",
+    delayError: 1500,
+  });
+
+  const onSubmit = (values) => {
+    console.log({ values });
+    signUp("Marco", "polo@marco.com");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        name="name"
+        placeholder="Name"
+        className={`block input form-control ${
+          errors.name ? "is-invalid" : ""
+        }`}
+        {...register("name", {
+          required: "Name is required",
+          pattern: {
+            value: /^.{5}$/i,
+            message: "Name needs at least 5 characters long",
+          },
+        })}
+      />
+      <p>{errors.name?.message}</p>
+
+      <input
+        name="email"
+        placeholder="Enter email"
+        className={`block input form-control ${
+          errors.email ? "is-invalid" : ""
+        }`}
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "Invalid email address format",
+          },
+        })}
+      />
+      <p>{errors.email?.message}</p>
+      <button className="block button is-primary" type="submit">
+        Sign in{" "}
+      </button>
+    </form>
+  );
+};
 
 export default SignUpView;
--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,3 @@
 export { default as SignInView } from "./SignIn/SignIn.view";
 export { default as SignUpView } from "./SignUp/SignUp.view";
+export { default as TempView } from "./Temp/Temp.view";
