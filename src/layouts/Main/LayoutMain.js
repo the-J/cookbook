5143f3365328ea4009543f1123d8734d369696e4
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-13 20:11:04 +0200

    CB-1 LayoutMain handle error notifications

--- src/layouts/Main/LayoutMain.js
+++ src/layouts/Main/LayoutMain.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navbar } from "../../components/";
+import { Notification, Navbar } from "../../components/";
 
 const LayoutMain = ({ children }) => (
   <div className="container">
     <Navbar />
     {children}
+    <Notification />
   </div>
 );
 
