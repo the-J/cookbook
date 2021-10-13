5d79e015a98997813ee16ecd6188110460b4cec7
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-13 17:34:49 +0200

    CB-1 utils.js - capitalize, capitalizeAll

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,22 +4,34 @@ export const isValidEmail = (email?: string): boolean => {
   }
 
   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email,
+    email
   );
 };
 
-export const isMinimumLength = (field: string, length: number): boolean =>
-  field.trim().length >= length;
+export const isMinimumLength = (field: string, length: number): boolean => {
+  return field.trim().length >= length;
+};
 
 export function getUniqueTimestamp(): number {
   return Date.now() + Math.random();
 }
+
+export const capitalize = (x: string): string => {
+  return x.replace(/^\w/, (c) => c.toUpperCase());
+};
+
+export const capitalizeAll = (x: string): string => {
+  return x
+    .trim()
+    .toLowerCase()
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+};
 
 // date format: "2021-02-05"
 export function getCurrentDate(): string {
   const date = new Date();
   const dateString = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
     .toISOString()
-    .split('T')[0];
+    .split("T")[0];
   return dateString;
 }
