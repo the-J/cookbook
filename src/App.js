4b36d59300f023a18a1c8c218b1f08b28a1b69a8
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-13 20:09:42 +0200

    CB-1 ErrorProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "App.styles.scss";
-import { AuthProvider } from "./context/auth.context";
 import AppRouter from "./router";
+import { AuthProvider } from "./context/auth/auth.context";
+import { ErrorProvider } from "./context/error.context";
 
 const App = () => (
   <React.StrictMode>
     <AuthProvider>
-      <AppRouter />
+      <ErrorProvider>
+        <AppRouter />
+      </ErrorProvider>
     </AuthProvider>
   </React.StrictMode>
 );
