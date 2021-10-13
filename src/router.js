028a2704f127a45669ac638961649cb41246bbf9
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-13 17:34:21 +0200

    CB-1 router.js

--- src/router.js
+++ src/router.js
@@ -1,47 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import { TempView, SignInView, SignUpView } from "views";
+import { HomeView, SignInView, SignUpView } from "views";
 
 const AppRouter = () => {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">Sign in</Link>
-          </li>
-          <li>
-            <Link to="/sign-up">Sign up </Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Route exact path="/">
-          <div>
-            <TempView />
-          </div>
-        </Route>
-        <Route path="/dashboard">
-          <div>Dashboard</div>
-        </Route>
-        <Route path="/sign-in">
-          <SignInView />
-        </Route>
-        <Route path="/sign-up">
-          <SignUpView />
-        </Route>
-      </div>
+      <Route exact path="/">
+        <HomeView />
+      </Route>
+      <Route path="/sign-in">
+        <SignInView />
+      </Route>
+      <Route path="/sign-up">
+        <SignUpView />
+      </Route>
     </Router>
   );
 };
