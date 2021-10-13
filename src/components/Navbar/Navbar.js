7785a8be58b1f4b5ef39dc468a7e4f12270cc41c
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-13 17:32:12 +0200

    CB-1 Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,56 +1,52 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Navbar = () => {
   const [openState, setOpenState] = useState(false);
-
-  const handleChangeState = () => setOpenState(!openState);
+  const handleChangeState = () => {
+    setOpenState(!openState);
+  };
 
   return (
-    <nav
-      className="navbar"
-      role="navigation"
-      aria-label="main navigation"
-      onClick={handleChangeState}
-    >
-      <a
-        role="button"
-        className={openState ? "navbar-burger is-active" : "navbar-burger"}
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-      </a>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        {/*<a className="navbar-item" href="https://bulma.io">*/}
+        {/*  <img*/}
+        {/*    src="https://bulma.io/images/bulma-logo.png"*/}
+        {/*    width="112"*/}
+        {/*    height="28"*/}
+        {/*  />*/}
+        {/*</a>*/}
 
-      {/*<div*/}
-      {/*  id="navbarBasicExample"*/}
-      {/*  className={openState ? "navbar-menu is-active" : "navbar-menu "}*/}
-      {/*>*/}
-      {/*  <div className="navbar-start">*/}
-      {/*    <a className="navbar-item">Settings</a>*/}
-      {/*    <hr className="navbar-divider" />*/}
-      {/*    <a className="navbar-item">Log out</a>*/}
-      {/*  </div>*/}
+        <a
+          role="button"
+          className={`navbar-burger ${openState && "is-active"}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={handleChangeState}
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
 
-      {/*  <div className="navbar-end">*/}
-      {/*    <div className="navbar-item">*/}
-      {/*      <div className="buttons">*/}
-      {/*        <a className="button is-primary">*/}
-      {/*          <strong>Sign up</strong>*/}
-      {/*        </a>*/}
-      {/*        <a className="button is-light">Log in</a>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className={`navbar-menu ${openState && "is-active"}`}>
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+        </div>
 
-      {/*<div>*/}
-      {/*  <button className="button">button</button>*/}
-      {/*  <button className="button">button</button>*/}
-      {/*  <button className="button">button</button>*/}
-      {/*</div>*/}
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
