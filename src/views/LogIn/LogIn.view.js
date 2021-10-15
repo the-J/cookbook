9e2a0fa9db0abcf72c8619c57290ec7e78413e59
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-15 10:06:09 +0200

    CB-1 LogInView

--- src/views/LogIn/LogIn.view.js
+++ src/views/LogIn/LogIn.view.js
@@ -1,62 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import { useForm } from "react-hook-form";
+import { useAuthContext } from "../../context/auth/auth.context";
+import { useError } from "../../context/error.context";
+import { logIn } from "../../auth/authUser";
+import { LayoutMain } from "../../layouts";
+
+const initState = {
+  email: "",
+  password: "",
+};
 
 const LogInView = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    defaultValues: {
-      name: "Name",
-      email: "Email",
-    },
-    criteriaMode: "all",
-    delayError: 1500,
-  });
+  const { initializeUser } = useAuthContext();
+  let history = useHistory();
+  const { addError } = useError();
 
-  const onSubmit = (values) => {
-    console.log({ values });
+  const [isValid, setIsValid] = useState(true);
+  const [validationErrors, setValidationError] = useState(initState);
+  const [fieldValues, setFieldValues] = useState(initState);
+
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  const checkIsValid = () => {
+    const isValid = Object.values(validationErrors).some((x) => !x);
+    setIsValid(isValid);
   };
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="name"
-        placeholder="Name"
-        className={`block input form-control ${
-          errors.name ? "is-invalid" : ""
-        }`}
-        {...register("name", {
-          required: "Name is required",
-          pattern: {
-            value: /^.{5}$/i,
-            message: "Name needs at least 5 characters long",
-          },
-        })}
-      />
-      <p>{errors.name?.message}</p>
+  useEffect(() => {
+    checkIsValid();
+  }, [validationErrors]);
 
-      <input
-        name="email"
-        placeholder="Enter email"
-        className={`block input form-control ${
-          errors.email ? "is-invalid" : ""
-        }`}
-        {...register("email", {
-          required: "Email is required",
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-            message: "Invalid email address format",
-          },
-        })}
-      />
-      <p>{errors.email?.message}</p>
-      <button className="block button is-primary" type="submit">
-        Sign in{" "}
-      </button>
-    </form>
+  const onSubmit = (e) => {
+    console.log("onsubmit");
+    e.preventDefault();
+    logIn(fieldValues.email, fieldValues.password)
+      .then(async () => {
+        await initializeUser();
+        history.replace(from);
+      })
+      .catch((err) => addError(err, "AWS err"));
+  };
+
+  // validation:
+  // trim
+  // check some shit
+  // go
+
+  const validateValue = (e) => {
+    // const eValue = e.target.value
+    console.log(e.target);
+    const eName = e.target.name;
+    switch (eName) {
+      case "email":
+        setValidationError({
+          ...validationErrors,
+          [eName]: e.target.validationMessage,
+        });
+        break;
+      case "password":
+        setValidationError({
+          ...validationErrors,
+          [eName]: e.target.validationMessage,
+        });
+        break;
+      default:
+        return;
+    }
+  };
+  return (
+    <LayoutMain>
+      <div className="columns">
+        <div className="column" />
+        <div className="column">
+          <form onSubmit={onSubmit} className="mx-4">
+            <input
+              required
+              placeholder="Email"
+              type="email"
+              name="email"
+              onChange={(e) =>
+                setFieldValues((prevState) => ({
+                  ...prevState,
+                  email: e.target.value,
+                }))
+              }
+              className={`block input form-control ${
+                validationErrors.email ? "is-invalid" : ""
+              }`}
+            />
+            {validationErrors.email && <p>{validationErrors.email}</p>}
+
+            <input
+              required
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={(e) =>
+                setFieldValues((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+              className={`block input form-control ${
+                validationErrors.password ? "is-invalid" : ""
+              }`}
+            />
+            {validationErrors.password && <p>{validationErrors.password}</p>}
+            <button
+              className="block button is-primary"
+              type="submit"
+              // disabled={isValid}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="column" />
+      </div>
+    </LayoutMain>
   );
 };
 
