4e9a48efcac6e3bf5bd798fd696f69c00b4b8eb4
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-15 17:00:31 +0200

    CB-1 router - PriveteRoute component

--- src/router.js
+++ src/router.js
@@ -1,42 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  Redirect,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import { HomeView, LogInView, SignUpView } from "views";
-// import { useAuthContext } from "src/contextauth/auth.context";
-// const PrivateRoute = ({ children, ...rest }) => {
-//   const auth = useAuthContext();
-//   return (
-//     <Route
-//       {...rest}
-//       render={({ location }) =>
-//         auth.user ? (
-//           children
-//         ) : (
-//           <Redirect
-//             to={{
-//               pathname: "/login",
-//               state: { from: location },
-//             }}
-//           />
-//         )
-//       }
-//     />
-//   );
-// };
+import { useAuthContext } from "./context/auth/auth.context";
+
+const PrivateRoute = ({ children, path }) => {
+  const { state } = useAuthContext();
+
+  return (
+    <Route path={path} exact>
+      {state.user ? children : <Redirect to={{ pathname: "/log-in" }} />}
+    </Route>
+  );
+};
+
 const AppRouter = () => {
   return (
     <Router>
-      <Route exact path="/">
+      <PrivateRoute path="/">
         <HomeView />
-      </Route>
-      <Route path="/log-in">
+      </PrivateRoute>
+
+      <Route exact path="/log-in">
         <LogInView />
       </Route>
-      <Route path="/sign-up">
+      <Route exact path="/sign-up">
         <SignUpView />
       </Route>
     </Router>
