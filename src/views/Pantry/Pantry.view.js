9da04612d53055d81e8b8a48a797b7d9211cd33a
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-17 21:59:06 +0200

    CB-1 PantryView

---
+++ src/views/Pantry/Pantry.view.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+import { LayoutMain } from "../../layouts";
+import { Card, Modal } from "../../components";
+
+const PantryView = () => {
+  const [openModalState, setOpenModalState] = useState(false);
+  return (
+    <>
+      <Modal
+        title="new modal"
+        openState={openModalState}
+        closeModal={() => setOpenModalState(false)}
+        saveChanges={() => {}}
+      />
+      <LayoutMain>
+        <div
+          className="
+            column
+            is-10
+            is-offset-1
+            is-flex
+            is-justify-content-flex-start
+            is-align-items-center
+            p-0
+            mb-2
+        "
+        >
+          <div style={{ minHeight: "50px" }}>
+            <button
+              className="button is-success"
+              type="button"
+              onClick={() => {
+                console.log("button");
+                setOpenModalState(true);
+              }}
+            >
+              <span className="icon">
+                <FontAwesomeIcon icon={faPlus} />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div className="columns">
+          <div
+            className="
+            column
+            is-10
+            is-offset-1
+            is-flex
+            is-flex-wrap-wrap
+            is-justify-content-center
+            is-align-items-center
+            p-0
+        "
+          >
+            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
+              (el, i) => (
+                <Card name={"name " + i} key={i} />
+              )
+            )}
+          </div>
+        </div>
+      </LayoutMain>
+    </>
+  );
+};
+
+export default PantryView;
--- src/views/index.js
+++ src/views/index.js
@@ -2,3 +2,4 @@ export { default as LogInView } from "./LogIn/LogIn.view";
 export { default as SignUpView } from "./SignUp/SignUp.view";
 export { default as HomeView } from "./Home/Home.view";
 export { default as RecipesView } from "./Recipes/Recipes.view";
+export { default as PantryView } from "./Pantry/Pantry.view";
