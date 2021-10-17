43d9f36d24dfff2f4037798ec376a0153c8f7f64
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-17 21:58:11 +0200

    CB-1 export new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Recipe } from "./Recipe/Recipe";
 export { default as RecipeList } from "./RecipeList/RecipeList";
 export { default as Loader } from "./Loader/Loader";
 export { default as Notification } from "./Notification/Notification";
+export { default as Card } from "./Card/Card";
+export { default as Modal } from "./Modal/Modal";
