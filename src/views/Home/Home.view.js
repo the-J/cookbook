f0172b0bb24488d4db03a174a2b20ea26795bc48
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-23 17:36:26 +0200

    CB-1 clear Home component

--- src/views/Home/Home.view.js
+++ src/views/Home/Home.view.js
@@ -1,81 +1,12 @@
 import React from "react";
-import { Layout } from "layouts";
-import { useAuthContext } from "../../context/auth/auth.context";
+import { Redirect } from "react-router-dom";
+
 import { LayoutMain } from "../../layouts";
-const HomeView = () => {
-  // const [blogName, setBlogName] = useState("");
-  // const [blogsList, setBlogsList] = useState([]);
-  //
-  // const getBlogPosts = async () => {
-  //   const blogPosts = await API.graphql(graphqlOperation(listBlogs));
-  //   const posts = blogPosts.data.listBlogs.items;
-  //   setBlogsList(posts);
-  // };
-  //
-  // useEffect(() => {
-  //   getBlogPosts();
-  // }, []);
-  //
-  // const addBlog = async () => {
-  //   const newBlog = {
-  //     id: uuidv4(),
-  //     name: blogName,
-  //   };
-  //   const newBlogsList = [newBlog, ...blogsList];
-  //   setBlogsList(newBlogsList);
-  //   setBlogName("");
-  //
-  //   await API.graphql(
-  //     graphqlOperation(createBlog, { input: { name: blogName } })
-  //   );
-  // };
-  //
-  // useEffect(() => {
-  //   const apiSub = API.graphql(graphqlOperation(onCreateBlog)).subscribe({
-  //     next: (newPostData) => {
-  //       const newPost = newPostData.value.data.onCreateBlog;
-  //       const newBlogsList = [newPost, ...blogsList];
-  //       const blogIds = blogsList.map(({ id }) => id);
-  //
-  //       if (!blogIds.includes(newPost.id)) {
-  //         setBlogsList(newBlogsList);
-  //       }
-  //     },
-  //     error: (errorValue) => {
-  //       console.log({ errorValue });
-  //     },
-  //   });
-  //
-  //   return () => {
-  //     apiSub.unsubscribe();
-  //   };
-  // }, [blogsList]);
 
-  const auth = useAuthContext();
-  console.log(auth);
-
-  return (
-    <LayoutMain>
-      <div className="container block">
-        <div className="columns">
-          <div className="column is-6 is-offset-2">
-            <input
-              name="todo"
-              className="input is-warning is-large"
-              type="text"
-              value={"blogName"}
-              placeholder="Add TODO"
-            />
-          </div>
-          <div className="column is-2 is-flex is-justify-content-center">
-            <button type="submit" className="button is-large is-warning">
-              Add przepis
-            </button>
-          </div>
-        </div>
-      </div>
-    </LayoutMain>
-  );
-};
+const HomeView = () => (
+  <LayoutMain>
+    <Redirect to="/pantry" />
+  </LayoutMain>
+);
 
 export default HomeView;
