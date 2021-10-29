d05165d303b7809ac65965912824a382988da696
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-29 15:37:58 +0200

    CB-1 change icon package

--- src/views/Pantry/Pantry.view.js
+++ src/views/Pantry/Pantry.view.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { DataStore } from "@aws-amplify/datastore";
 import { nanoid } from "nanoid";
 
@@ -8,6 +6,7 @@ import { Stock } from "../../models";
 import { LayoutMain } from "../../layouts";
 import { Card, ImageCapture, Modal } from "../../components";
 import { useAuthContext } from "../../context/auth/auth.context";
+import { AiOutlinePlus, BiImageAdd } from "react-icons/all";
 
 const initialState = {
   name: "",
@@ -20,6 +19,7 @@ const PantryView = () => {
   const [openModalState, setOpenModalState] = useState(false);
   const [stock, setStock] = useState([]);
   const [newStock, setNewStock] = useState(initialState);
+  const [displayImageCapture, setDisplayImageCapture] = useState(false);
 
   useEffect(() => {
     fetchStock();
@@ -50,7 +50,7 @@ const PantryView = () => {
       ...newStock,
       quantity: Number(newStock.quantity),
       id: nanoid(),
-      creatorID: state.userConfig.username,
+      creatorID: state.user.id,
       createdAt: new Date().toISOString(),
     };
 
@@ -80,14 +80,28 @@ const PantryView = () => {
       >
         <h3>All fields required</h3>
         <br />
-        <input
-          name="name"
-          className="input is-large block"
-          type="text"
-          value={newStock.name}
-          placeholder="Name"
-          onChange={onChange}
-        />
+        {displayImageCapture && (
+          <ImageCapture startCamera={displayImageCapture} />
+        )}
+        <div className="field is-grouped">
+          <input
+            name="name"
+            className="input is-large mr-3"
+            type="text"
+            value={newStock.name}
+            placeholder="Name"
+            onChange={onChange}
+          />
+          <button
+            onClick={() => setDisplayImageCapture(!displayImageCapture)}
+            // className={`button is-primary ${isLoggingIn && "is-loading"}`}
+            className={`button is-success is-large`}
+            type="submit"
+            // disabled={!isValid}
+          >
+            <BiImageAdd />
+          </button>
+        </div>
         <input
           name="quantity"
           className="input is-large block"
@@ -126,40 +140,39 @@ const PantryView = () => {
               }}
             >
               <span className="icon">
-                <FontAwesomeIcon icon={faPlus} />
+                <AiOutlinePlus />
               </span>
             </button>
           </div>
         </div>
-        <ImageCapture />
-        {/*<div className="columns">*/}
-        {/*  <div*/}
-        {/*    className="*/}
-        {/*    column*/}
-        {/*    is-10*/}
-        {/*    is-offset-1*/}
-        {/*    is-flex*/}
-        {/*    is-flex-wrap-wrap*/}
-        {/*    is-justify-content-center*/}
-        {/*    is-align-items-center*/}
-        {/*    p-0*/}
-        {/*"*/}
-        {/*  >*/}
-        {/*    {stock.length > 0 ? (*/}
-        {/*      stock.map((el, i) => (*/}
-        {/*        <Card*/}
-        {/*          name={el.name}*/}
-        {/*          date={el.createdAt}*/}
-        {/*          description={el.description}*/}
-        {/*          quantity={el.quantity}*/}
-        {/*          key={i}*/}
-        {/*        />*/}
-        {/*      ))*/}
-        {/*    ) : (*/}
-        {/*      <div>Nothing here</div>*/}
-        {/*    )}*/}
-        {/*  </div>*/}
-        {/*</div>*/}
+        <div className="columns">
+          <div
+            className="
+            column
+            is-10
+            is-offset-1
+            is-flex
+            is-flex-wrap-wrap
+            is-justify-content-center
+            is-align-items-center
+            p-0
+        "
+          >
+            {stock.length > 0 ? (
+              stock.map((el, i) => (
+                <Card
+                  name={el.name}
+                  date={el.createdAt}
+                  description={el.description}
+                  quantity={el.quantity}
+                  key={i}
+                />
+              ))
+            ) : (
+              <div>Nothing here</div>
+            )}
+          </div>
+        </div>
       </LayoutMain>
     </>
   );
