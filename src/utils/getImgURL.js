bd7df1b03739eb101396b8e7f9f3499eba4cf6b6
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-30 15:56:25 +0200

    CB-1 getImageURL format

--- src/utils/getImgURL.js
+++ src/utils/getImgURL.js
@@ -1,8 +1,8 @@
-import {Storage} from "aws-amplify";
+import { Storage } from "aws-amplify";
 
 const getImgURL = async (imgKey) => {
   try {
-    return await Storage.get(imgKey, {expires: 60});
+    return await Storage.get(imgKey, { expires: 60 });
   } catch (err) {
     // @TODO handle err
     console.log("get img err:", err);
