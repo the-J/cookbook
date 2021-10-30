cb6a20c0b8a47b22c358bdc00726a2d53c0f61a0
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-10-30 14:35:23 +0200

    CB-1 PrivateRoute - will work on this later

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -25,11 +25,11 @@ const PrivateRoute = ({ children, path }) => {
 
   // @TODO still don't like this mess
   // @TODO some info if fail or reload
-  return (
+  return user ? (
     <Route path={path} exact>
       {user ? children : <Redirect to={{ pathname: "/log-in" }} />}
     </Route>
-  );
+  ) : null;
 };
 
 export default PrivateRoute;
