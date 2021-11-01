52a62dcd06f08b9883e834ba606d127225ae81da
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-11-01 20:02:15 +0100

    CB-1 remove unused code

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,4 @@
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as Recipe } from "./Recipe/Recipe";
-export { default as RecipeList } from "./RecipeList/RecipeList";
-export { default as Loader } from "./Loader/Loader";
 export { default as Notification } from "./Notification/Notification";
 export { default as Card } from "./Card/Card";
 export { default as ImageCapture } from "./ImageCapture/ImageCapture";
