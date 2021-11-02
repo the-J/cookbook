65a36291096cf12732e4db00247d675a5292eb62
Juliusz Jakubowski <juliusz.jakubowski@noreply.com>
2021-11-02 19:28:05 +0100

    CB-1 useUpload - share upload state as well

--- src/hooks/useUploadImage.js
+++ src/hooks/useUploadImage.js
@@ -1,11 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Storage } from "aws-amplify";
 import { nanoid } from "nanoid";
+import { useNotif } from "../context/notif/notifications.context";
 
 const useUploadPhoto = () => {
+  const { pushNotif } = useNotif();
+
   const [imgName, setImgName] = useState(null);
+  const [imgUploading, setImgUploading] = useState(false);
 
   const uploadImage = async (image) => {
+    setImgUploading(true);
+
     // @INFO conversion to blob for upload to S3
     const base64 = await fetch(image);
     const imageAsBlob = await base64.blob();
@@ -15,14 +21,19 @@ const useUploadPhoto = () => {
     const setNeaBlobHeader = new Blob([imageAsBlob], { type: "image/png" });
 
     const imgKey = "pantry-image" + nanoid() + ".png";
+
     // @TODO private or public? use user auth probably
+    // @TODO run lambda to reduce weight of img
     Storage.put(imgKey, setNeaBlobHeader, {
       level: "public",
       contentType: "image/png",
-    }).then((result) => setImgName(result.key));
+    })
+      .then((result) => setImgName(result.key))
+      .catch((error) => pushNotif(error.message, "err"))
+      .finally(() => setImgUploading(false));
   };
 
-  return { imgName, uploadImage };
+  return { imgName, imgUploading, uploadImage };
 };
 
 export default useUploadPhoto;
